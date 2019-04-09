8c72f82d1d2d6e19f78a72c3d9e3621ca846db61
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-10 00:03:08 +0300

    updated movieInstanceController

--- src/main/java/ro/fiipractic/mycinema/controllers/MovieInstanceController.java
+++ src/main/java/ro/fiipractic/mycinema/controllers/MovieInstanceController.java
@@ -1,9 +1,46 @@
 package ro.fiipractic.mycinema.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ro.fiipractic.mycinema.dtos.MovieInstanceDto;
+import ro.fiipractic.mycinema.entities.MovieInstance;
+import ro.fiipractic.mycinema.services.MovieInstanceService;
+import ro.fiipractic.mycinema.services.MovieService;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/movie-instance")
+@RequestMapping(value = "/api/movie-instances")
 public class MovieInstanceController {
+
+    @Autowired
+    MovieInstanceService movieInstanceService;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @GetMapping
+    public List<MovieInstance> getAllMovieInstances(){
+        return movieInstanceService.getAllMovieInstaces();
+    }
+
+    @GetMapping("/{id}")
+    public MovieInstance getMovieInstanceById(@PathVariable Long id) {
+        return movieInstanceService.getMovieInstanceById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieInstance> saveMovieInstance(@RequestBody MovieInstanceDto movieInstanceDto) throws URISyntaxException {
+        MovieInstance movieInstance = movieInstanceService.saveMovieInstance(modelMapper.map(movieInstanceDto,MovieInstance.class));
+        return ResponseEntity.created(new URI("api/movie-instances/" + movieInstance.getId())).body(movieInstance);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public void deleteMovieInstance(@PathVariable Long id) {
+        //movieInstanceService.deleteMovieInstance(id);
+    }
 }
