156680139100644276d3a80e753af1cb25900cc3
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-09 22:34:43 +0300

    implemented getAll and saveMovie

--- src/main/java/ro/fiipractic/mycinema/services/MovieService.java
+++ src/main/java/ro/fiipractic/mycinema/services/MovieService.java
@@ -1,4 +1,11 @@
 package ro.fiipractic.mycinema.services;
 
+import ro.fiipractic.mycinema.entities.Movie;
+
+import java.util.List;
+
 public interface MovieService {
+
+    List<Movie> getAll();
+    Movie saveMovie(Movie movie);
 }
--- src/main/java/ro/fiipractic/mycinema/services/impl/MovieServiceImpl.java
+++ src/main/java/ro/fiipractic/mycinema/services/impl/MovieServiceImpl.java
@@ -2,8 +2,11 @@ package ro.fiipractic.mycinema.services.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ro.fiipractic.mycinema.entities.Movie;
 import ro.fiipractic.mycinema.repositories.MovieRepository;
 import ro.fiipractic.mycinema.services.MovieService;
+
+import java.util.List;
 
 @Service
 public class MovieServiceImpl implements MovieService {
@@ -11,5 +14,13 @@ public class MovieServiceImpl implements MovieService {
     @Autowired
     MovieRepository movieRepository;
 
+    @Override
+    public List<Movie> getAll() {
+        return movieRepository.findAll();
+    }
 
+    @Override
+    public Movie saveMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
 }
