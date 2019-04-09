0182eb2263961f3d31dd0732ab8664a1f5c1edeb
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-09 23:41:37 +0300

    added MovieInstance services

---
+++ src/main/java/ro/fiipractic/mycinema/services/MovieInstanceService.java
@@ -0,0 +1,13 @@
+package ro.fiipractic.mycinema.services;
+
+import ro.fiipractic.mycinema.entities.MovieInstance;
+
+import java.util.List;
+
+public interface MovieInstanceService {
+
+    List<MovieInstance> getAllMovieInstaces();
+    MovieInstance getMovieInstanceById(Long id);
+    MovieInstance saveMovieInstance(MovieInstance movieInstance);
+    void deleteMovieInstance(Long id);
+}
---
+++ src/main/java/ro/fiipractic/mycinema/services/impl/MovieInstanceServiceImpl.java
@@ -0,0 +1,38 @@
+package ro.fiipractic.mycinema.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ro.fiipractic.mycinema.entities.Movie;
+import ro.fiipractic.mycinema.entities.MovieInstance;
+import ro.fiipractic.mycinema.repositories.MovieInstanceRepository;
+import ro.fiipractic.mycinema.services.MovieInstanceService;
+
+import java.util.List;
+
+@Service
+public class MovieInstanceServiceImpl implements MovieInstanceService {
+
+    @Autowired
+    MovieInstanceRepository movieInstanceRepository;
+
+    @Override
+    public List<MovieInstance> getAllMovieInstaces() {
+        return movieInstanceRepository.findAll();
+    }
+
+    @Override
+    public MovieInstance getMovieInstanceById(Long id) {
+        return movieInstanceRepository.getOne(id);
+    }
+
+    @Override
+    public MovieInstance saveMovieInstance(MovieInstance movieInstance) {
+        return movieInstanceRepository.save(movieInstance);
+    }
+
+    @Override
+    public void deleteMovieInstance(Long id) {
+        movieInstanceRepository.deleteById(id);
+        // probably not completed
+    }
+}
