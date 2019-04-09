f832a7dc46487bdd48ad955f9a7fbc37c72f0e1b
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-09 23:55:27 +0300

    refactored dtos

--- src/main/java/ro/fiipractic/mycinema/dtos/MovieInstanceDto.java
+++ src/main/java/ro/fiipractic/mycinema/dtos/MovieInstanceDto.java
@@ -10,6 +10,12 @@ public class MovieInstanceDto {
 
     private Integer availableSeats;
 
+    private Integer cinema_id;
+
+    private Integer movie_id;
+
+    private Integer movie_room_id;
+
     public Long getId() {
         return id;
     }
@@ -40,5 +46,29 @@ public class MovieInstanceDto {
 
     public void setAvailableSeats(Integer availableSeats) {
         this.availableSeats = availableSeats;
+    }
+
+    public Integer getCinema_id() {
+        return cinema_id;
+    }
+
+    public void setCinema_id(Integer cinema_id) {
+        this.cinema_id = cinema_id;
+    }
+
+    public Integer getMovie_id() {
+        return movie_id;
+    }
+
+    public void setMovie_id(Integer movie_id) {
+        this.movie_id = movie_id;
+    }
+
+    public Integer getMovie_room_id() {
+        return movie_room_id;
+    }
+
+    public void setMovie_room_id(Integer movie_room_id) {
+        this.movie_room_id = movie_room_id;
     }
 }
--- src/main/java/ro/fiipractic/mycinema/dtos/ReservationDto.java
+++ src/main/java/ro/fiipractic/mycinema/dtos/ReservationDto.java
@@ -5,4 +5,40 @@ public class ReservationDto {
     private Long id;
 
     private Integer numberOfTickets;
+
+    private Integer customer_id;
+
+    private Integer movie_instance_id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNumberOfTickets() {
+        return numberOfTickets;
+    }
+
+    public void setNumberOfTickets(Integer numberOfTickets) {
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    public Integer getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(Integer customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public Integer getMovie_instance_id() {
+        return movie_instance_id;
+    }
+
+    public void setMovie_instance_id(Integer movie_instance_id) {
+        this.movie_instance_id = movie_instance_id;
+    }
 }
