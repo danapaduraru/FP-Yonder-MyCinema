3e49ca1ebc83d4f350bf2e31a8408351cf3b3423
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-12 18:26:38 +0300

    added custom exceptions

---
+++ src/main/java/ro/fiipractic/mycinema/exceptions/MyCinemaGenericException.java
@@ -0,0 +1,12 @@
+package ro.fiipractic.mycinema.exceptions;
+
+public class MyCinemaGenericException extends Exception {
+
+    public MyCinemaGenericException(String message) {
+        super(message);
+    }
+
+    public MyCinemaGenericException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
